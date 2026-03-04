d7557af7b2be8d6ea0ed0dcb576e46bffd3cbbc3
LameesAlharbi <harbilamees86@gmail.com>
2026-03-04 12:57:48 +0300

    todo1v2

--- 5-6-react-styling-fetch-data/src/components/SearchBar.jsx
+++ 5-6-react-styling-fetch-data/src/components/SearchBar.jsx
@@ -4,16 +4,13 @@ import { Form, InputGroup } from "react-bootstrap";
 function SearchBar({ searchTerm, onSearchChange }) {
   return (
     // TODO 1.2: Set wrapper div className EXACTLY to "mb-4"
-    <div className="mb-4">
-      <InputGroup>
-        <InputGroup.Text>Search</InputGroup.Text>
-        <Form.Control
-          type="text"
-          placeholder="Search by name..."
-          value={searchTerm}
-          onChange={(e) => onSearchChange(e.target.value)}
-        />
-      </InputGroup>
+   <div className="mb-4">
+      <input
+        className="form-control"
+        value={searchTerm}
+        onChange={(e) => onSearchChange(e.target.value)}
+        placeholder="Search users by name..."
+      />
     </div>
   );
 }
